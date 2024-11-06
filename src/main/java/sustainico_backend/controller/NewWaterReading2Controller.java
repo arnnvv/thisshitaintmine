41bb1e22c3bf9ac0cc9ddaf1a7c16afa38606e89
arnnvv <arnav.900@protonmail.com>
2024-11-06 11:47:03 +0530

    chk

--- src/main/java/sustainico_backend/controller/NewWaterReading2Controller.java
+++ src/main/java/sustainico_backend/controller/NewWaterReading2Controller.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/newWaterReading2")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class NewWaterReading2Controller {
     @Autowired
     private NewWaterReading2Service service;
